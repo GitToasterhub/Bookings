1c42e3d303c15971b40970e7263edfb3542a0e39
GitToasterhub <abulfazahmadov@gmail.com>
2017-09-26 22:58:51 +0300

    Css and JS files separated.Added delete feature to bookings

---
+++ src/main/resources/static/js/add.js
@@ -0,0 +1,22 @@
+var AddForm = React.createClass({
+ render: function() {   
+    return (
+       <div>
+            <form id="form" action="/addBooking" method="post" >
+            <div><label> Start date : <input type="text" name="startDate"/> </label></div>
+            <div><label> End date: <input type="text" name="endDate"/> </label></div>
+            
+            <div><label> Client's name: <input type="text" name="clientName"/> </label></div>
+            <div><label> Room type: <input type="text" name="roomType" id="roomType"/> </label></div>
+            
+            <div><label> Comment: <input type="text" name="comment"/> </label></div>
+            <div><input id="submit" type="submit" value="Add new booking"/></div>
+        </form>
+       </div>
+     )
+}      
+});
+      ReactDOM.render(
+       <AddForm/>,
+        document.getElementById('AddForm')
+      );
