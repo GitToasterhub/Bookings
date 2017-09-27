21888c8d436d4bcac5f405256b8c6ef5624f9c54
GitToasterhub <abulfazahmadov@gmail.com>
2017-09-27 10:56:44 +0300

    Update bookings.js

--- src/main/resources/static/js/bookings.js
+++ src/main/resources/static/js/bookings.js
@@ -1,3 +1,66 @@
-/**
- * 
- */
+
+var BookingsList = React.createClass({
+
+  getInitialState: function() {
+    return {
+      bookings: []
+    }
+  },
+
+  componentDidMount: function() {
+    var _this = this;
+    this.serverRequest = 
+      axios
+        .get("/allBookings")
+        .then(function(result) {    
+          _this.setState({
+            bookings: result.data.bookings
+          });
+        })
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+ handleDeleteClick: function(id){
+  this.serverRequest = 
+      axios
+        .get("/deleteBooking"+id)
+        .then(function (response) {
+            console.log("An item was deleted");
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+},
+
+  render: function() {
+   
+    return (
+
+      <div >
+        <h1>Bookings</h1>
+        {this.state.bookings.map(function(booking,i) {
+          return (
+            <div  className="card" key="{booking.id}">
+               <p>Room type: {booking.roomType}</p>
+              <p>Order deatils: {booking.clientName}: {booking.comment}</p>
+               <p>From {booking.startDate}: to{booking.endDate}</p>
+               <p>Order id:{booking.id}</p>
+              <button type="button" onclick={this.props.handleDeleteClick.bind(this,booking.id)}>Delete</button>
+              <br></br>
+              <br></br>
+            </div>
+            
+          );
+          
+        })}
+      </div>
+    )
+  }
+});
+      
+      ReactDOM.render(
+       <BookingsList/>,
+        document.getElementById('root')
+      );
